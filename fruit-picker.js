2e7ae3600106a17ce169ea1f10cea6f04752a205
mhmmd <mhmmd.rezaei4@gmail.com>
2023-03-06 10:41:20 +0330

    Function: onSuccess

--- fruit-picker.js
+++ fruit-picker.js
@@ -1,1 +1,10 @@
 /// difficult 😯😑🤔
+
+// import { notify } from "./notifier";
+// import { order } from "./grocer";
+
+function onSuccess() {
+  return notify({ message: "SUCCESS" });
+}
+
+// console.log(onSuccess());
